944c97c8ba1399896f69bf3c59ee35b53aa5dcd2
Susumu Tokumoto <tokumoto.susumu@nifty.com>
2017-10-10 01:03:49 +0900

    Added functions

--- elixir/lib/lifegame.ex
+++ elixir/lib/lifegame.ex
@@ -1,2 +1,12 @@
 defmodule LifeGame do
+  defstruct size: 0, board: [{0, 0}]
+
+  def newGeneration (lifegame = %LifeGame{}) do
+  end
+
+  def countAlive (lifegame = %LifeGame{}, x, y) do
+  end
+
+  def isAlive (lifegame = %LifeGame{}, x, y) do
+  end
 end
