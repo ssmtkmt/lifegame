0c38d8ee3056246d5b5c91c3b0a7c0ebcb4b2f19
Tokumoto, Susumu <tokumoto.susumu@jp.fujitsu.com>
2017-10-11 00:25:10 +0900

    Completed implementation of elixir version

--- elixir/lib/lifegame.ex
+++ elixir/lib/lifegame.ex
@@ -1,12 +1,65 @@
 defmodule LifeGame do
-  defstruct size: 0, board: [{0, 0}]
+  defstruct size: 0, board: []
 
-  def newGeneration (lifegame = %LifeGame{}) do
+  def newGeneration(lifegame = %LifeGame{}) do
+    %LifeGame{size: lifegame.size,
+              board: (for x <- 0..lifegame.size-1, y <- 0..lifegame.size-1, isAlive(lifegame, x, y), do: {x,y})}
   end
 
-  def countAlive (lifegame = %LifeGame{}, x, y) do
+  def countAlive(lifegame = %LifeGame{}, x, y) do
+    neigh = for xx <- x-1..x+1, yy <- y-1..y+1, !(xx==x) or !(yy==y), do: {xx, yy}
+    lifegame.board
+    |> Enum.filter(fn elm -> Enum.member?(neigh, elm) end)
+    |> Kernel.length
   end
 
-  def isAlive (lifegame = %LifeGame{}, x, y) do
+  def isAlive(lifegame = %LifeGame{}, x, y) do
+    numAlive = countAlive(lifegame, x, y)
+    nowAlive = Enum.member?(lifegame.board, {x, y})
+    cond do
+      nowAlive && numAlive != 2 && numAlive != 3
+        -> false
+      !nowAlive && numAlive == 3
+        -> true
+      true
+        -> nowAlive
+    end
   end
+
+  def putCell(lifegame = %LifeGame{}, x, y) do
+    if {x, y} in lifegame.board do
+      '*'
+    else
+      ' '
+    end
+  end
+
+  def printRow(lifegame = %LifeGame{}, row) do
+    IO.puts(for y <- 0..lifegame.size, do: putCell(lifegame, row, y))
+  end
+
+  def printBoard(lifegame = %LifeGame{}) do
+    for x <- 0..lifegame.size, do: printRow(lifegame, x)
+  end
+
+  def runLifegame(lifegame = %LifeGame{}) do
+    IEx.Helpers.clear()
+    printBoard(lifegame)
+    IO.puts("")
+    :timer.sleep(100)
+    runLifegame(newGeneration(lifegame))
+  end
+
+  def blinker do
+    %LifeGame{size: 5, board: [{1,1}, {1,2}, {1,3}]}
+  end
+
+  def pulsar do
+    %LifeGame{size: 17, board: [{7,8}, {7,9}, {7,10}, {9,9}, {11,8}, {11,9}, {11,10}]}
+  end
+
+  def main do
+    runLifegame(pulsar())
+  end
+
 end
--- elixir/test/lifegame_test.exs
+++ elixir/test/lifegame_test.exs
@@ -1,8 +1,9 @@
 defmodule LifeGameTest do
   use ExUnit.Case
   doctest LifeGame
+  import LifeGame
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "blinker" do
+    assert newGeneration(blinker()) == %LifeGame{size: 5, board: [{0,2}, {1,2}, {2,2}]}
   end
 end
